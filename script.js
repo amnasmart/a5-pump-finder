3cc795a5545dd07ac601e7e11e9b00e9cf6e915e
amnasmart <amnaadnan303@gmail.com>
2025-06-12 16:37:52 +0500

    Initial upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const signalBox = document.getElementById("signal-box");
+    signalBox.innerHTML = "<strong>Example:</strong> BTC is pumping!";
+});
