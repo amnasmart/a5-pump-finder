db733680a537829f037877d74a301e4cebc85114
amnasmart <amnaadnan303@gmail.com>
2025-06-21 15:08:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const signalBox = document.getElementById("signal-box");
-  fetch("/signals")
+  fetch("https://a5-pump-backend.vercel.app/signals")
     .then((response) => response.json())
     .then((data) => {
       signalBox.innerHTML = data.message;
